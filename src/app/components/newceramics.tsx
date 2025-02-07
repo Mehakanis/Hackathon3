45ce8fead262b25f278caebac1822c8ad344352b
Mehakanis <159849439+Mehakanis@users.noreply.github.com>
2025-02-07 21:44:46 +0500

    update

--- src/app/components/newceramics.tsx
+++ src/app/components/newceramics.tsx
@@ -7,13 +7,22 @@ import { newceramics } from "@/sanity/lib/queries";
 import Link from "next/link";
 import { Product } from "../types/product";
 import { handleAddToCart } from "../utils/handlers";
+import imageUrlBuilder from '@sanity/image-url';
+import { client } from '@/sanity/lib/client';
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
+const builder = imageUrlBuilder(client);
+
+function urlFor(source: SanityImageSource) {
+  return builder.image(source);
+}
 
 export type Data = {
   _id: string;
   name: string;
-  imageUrl: string; // Use the correct field name
+  imageUrl: string;
   price: number;
-  slug: { current: string }; // Slug object with current value
+  slug: { current: string };
 };
 
 export default function NewCeramics() {
@@ -23,12 +32,11 @@ export default function NewCeramics() {
     const fetchData = async () => {
       const result = await sanityFetch({ query: newceramics });
       console.log("Fetched data:", result); // Debugging line
-
       setData(result);
     };
 
     fetchData();
-  }, []); // Empty dependency array to run only once on mount
+  }, []);
 
   return (
     <section className="pt-[100px]">
@@ -37,12 +45,12 @@ export default function NewCeramics() {
       </div>
       <div className="flex flex-wrap justify-around mt-10 text-[#2A254B]">
         {data.map((product) => (
-          <div key={product._id} className="w-[280px] ">
+          <div key={product._id} className="w-[280px]">
             <Link href={`/product/${product.slug.current}`}>
               <div className="aspect-[2/2.5] relative group overflow-hidden">
                 {product.imageUrl ? (
                   <Image
-                    src={product.imageUrl} // Use the imageUrl field
+                    src={urlFor(product.imageUrl).url()}
                     alt={product.name}
                     layout="fill"
                     objectFit="cover"
@@ -52,7 +60,6 @@ export default function NewCeramics() {
                     <span className="text-gray-500">No Image</span>
                   </div>
                 )}
-                {/* Overlay with Add to Cart */}
                 <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
                   <button
                     className="bg-white text-black px-4 py-2 text-sm font-semibold hover:bg-gray-200 transition"
